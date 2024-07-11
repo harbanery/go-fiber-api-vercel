0813a05ebb59ad0e1578bc3bcff4dd23e1aee226
harbanery <ryusuf05@gmail.com>
2024-07-12 00:59:25 +0700

    fix: deployment error

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"gofiber-api-vercel/src/helpers"
 	"gofiber-api-vercel/src/routes"
 	"log"
-	"net/http"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
 
 func main() {
@@ -19,14 +17,4 @@ func main() {
 	routes.SetupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
-}
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	app := fiber.New()
-
-	configs.InitDB()
-	helpers.Migration()
-	routes.SetupRoutes(app)
-
-	adaptor.FiberApp(app)(w, r)
 }
